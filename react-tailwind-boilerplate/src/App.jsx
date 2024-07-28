4c6cf61c76ac5717dc5c0d54fe4681cc877b01f0
Nandinis1024 <nandini.sunil1024@gmail.com>
2024-07-28 15:45:22 +0530

    initialized the project

---
+++ react-tailwind-boilerplate/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div className="bg-background dark">
+    <h1 className="text-3xl font-bold underline text-red-400 text-center">
+      <Button>hello</Button>
+    </h1>
+    </div>
+  )
+}
+
+export default App
